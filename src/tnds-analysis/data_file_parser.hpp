8cf844136c6bec67afc04429723112a84b1c62da
diamant711 <53996598+diamant711@users.noreply.github.com>
2021-12-22 16:47:49 +0100

    Ready for fill_raw() implementation

--- src/tnds-analysis/data_file_parser.hpp
+++ src/tnds-analysis/data_file_parser.hpp
@@ -1,5 +1,7 @@
 #include "common_type.hpp"
 
+#include <unistd>
+
 #include <string>
 #include <fstream>
 
@@ -11,11 +13,26 @@ class data_file_parser {
 
     data_file_parser(data_type_t);
     ~data_file_parser();
+    
+    /*
+     *  check m_ready_to_read
+     *  read metadata
+     *  cicle to read *.fdt // <--- std::istream::read()
+     *                      // unsigned int n;
+     *                      // read(reinterpret_cast<char*>(&n), sizeof n)
+     *  and store into data m_raw
+    */
+    bool fill_raw();
+   
+    bool set_data(data&);
+
   private:
     data_type_t m_data_type;
+    data m_data;
     const unsigned int max_len_file_name = 20;
     std::string m_current_file_path;
     std::ofstream m_current_file;
+    bool m_ready_to_read = false;
 };
 
 data_file_parser::data_file_parser(data_type_t type) : m_data_type(type) {
@@ -27,4 +44,13 @@ data_file_parser::data_file_parser(data_type_t type) : m_data_type(type) {
 
 data_file_parser::~data_file_parser() {
   m_current_file.close();
+}
+
+bool data::set_data(data &dat){
+  if(dat.GetType() == m_data_type) {
+    m_data = dat;
+    m_ready_to_read = true;
+    return true;
+  } else
+    return false;
 }
