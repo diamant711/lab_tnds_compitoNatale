d80e5430daa0ddb517783d20b637ed5ce7001850
diamant711 <53996598+diamant711@users.noreply.github.com>
2021-12-22 13:18:25 +0100

    Started data_file_parser

--- src/tnds-analysis/data_file_parser.hpp
+++ src/tnds-analysis/data_file_parser.hpp
@@ -1,5 +1,28 @@
 #include "common_type.hpp"
 
-class data_file_parser {
+#include <string>
+#include <fstream>
 
+#include <cstdio> //sprintf
+
+class data_file_parser {
+  public:
+    enum data_type_t {ACCELEROMETER = 'a', CAMERA = 'i'};
+
+    data_file_parser(data_type_t);
+    ~data_file_parser();
+  private:
+    data_type_t m_data_type;
+    std::string m_current_file_path;
+    ofstream m_current_file;
 };
+
+data_file_parser::data_file_parser(data_type_t type) {
+  m_data_type = type;
+  ::sprintf(m_current_file_path, "data/%c/0.fdt", m_data_type);
+  m_current_file.open(m_current_file_path);
+}
+
+data_file_parser::~data_file_parser() {
+  m_current_file.close();
+}
