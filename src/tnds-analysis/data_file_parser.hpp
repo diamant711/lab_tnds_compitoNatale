30ff4ab2308736163e2588ecab6be5920b3e88d4
diamant711 <53996598+diamant711@users.noreply.github.com>
2021-12-22 17:36:58 +0100

    Partial-fix of data_file_parser.hpp

--- src/tnds-analysis/data_file_parser.hpp
+++ src/tnds-analysis/data_file_parser.hpp
@@ -7,9 +7,7 @@
 
 class data_file_parser {
   public:
-    enum data_type_t {ACCELEROMETER = 'a', CAMERA = 'i'};
-
-    data_file_parser(data_type_t);
+    data_file_parser(data::data_type_t);
     ~data_file_parser();
     
     /*
@@ -22,10 +20,10 @@ class data_file_parser {
     */
     bool fill_raw();
    
-    bool set_data(data&);
+    bool set_data(data &);
 
   private:
-    data_type_t m_data_type;
+    data::data_type_t m_data_type;
     data m_data;
     const unsigned int max_len_file_name = 20;
     std::string m_current_file_path;
@@ -33,7 +31,8 @@ class data_file_parser {
     bool m_ready_to_read = false;
 };
 
-data_file_parser::data_file_parser(data_type_t type) : m_data_type(type) {
+data_file_parser::data_file_parser(data::data_type_t type) {
+  m_data_type = type;
   char tmp[max_len_file_name];
   ::sprintf(tmp, "data/%c/0.fdt", m_data_type);
   m_current_file_path = tmp;
@@ -44,7 +43,7 @@ data_file_parser::~data_file_parser() {
   m_current_file.close();
 }
 
-bool data::set_data(data &dat){
+bool data_file_parser::set_data(data &dat){
   if(dat.GetType() == m_data_type) {
     m_data = dat;
     m_ready_to_read = true;
