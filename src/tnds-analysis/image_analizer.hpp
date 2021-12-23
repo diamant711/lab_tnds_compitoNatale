b029ddd981a31a930d2e47e88fa4349831533f03
diamant711 <53996598+diamant711@users.noreply.github.com>
2021-12-23 15:00:41 +0100

    Started image_analizer

--- src/tnds-analysis/image_analizer.hpp
+++ src/tnds-analysis/image_analizer.hpp
@@ -1,5 +1,19 @@
 #include "common_type.hpp"
-
+/*
 class image_analizer {
+  public:
+    image_analizer(data &);
+    ~image_analizer();
+  
+    bool find_center_of_mass(data::frame_data::frame);
 
+  private:
+    data m_data;
 };
+
+image_analizer::image_analizer(data &dat) {
+  m_data = dat;
+}
+
+image_analizer::~image_analizer() {}
+*/
