982dad2720de963f510368f8d835f4312f226d90
diamant711 <53996598+diamant711@users.noreply.github.com>
2021-12-29 16:13:16 +0100

    Stared fill_raw() method of data_file_parser (UNTESTED)

--- src/tnds-analysis/data_file_parser.hpp
+++ src/tnds-analysis/data_file_parser.hpp
@@ -20,27 +20,54 @@ class data_file_parser {
     */
     bool fill_raw();
    
-    void print();
-
   private:
     data m_data;
     const unsigned int max_len_file_name = 20;
     std::string m_current_file_path;
-    std::ofstream m_current_file;
+    std::ifstream m_current_file;
     bool m_ready_to_read = false;
     unsigned int m_tot_number_of_file;
 };
 
-data_file_parser::data_file_parser(data &dat) : m_data(dat) {
+data_file_parser::data_file_parser(data &dat) : m_data(dat) {}
+
+data_file_parser::~data_file_parser() {}
+
+bool data_file_parser::fill_raw() {
+  int file_to_read = 1;
   char tmp[max_len_file_name];
-  ::sprintf(tmp, "data/%c/0.fdt", m_data.GetType());
-  m_current_file_path = tmp;
-  m_current_file.open(m_current_file_path);
-}
-
-data_file_parser::~data_file_parser() {
-  m_current_file.close();
-}
-
-void data_file_parser::print(){
+  /* ---------error on the saved metadata file------------------- 
+  ::sprintf(tmp, "data/%c/metadata", m_data.GetType());
+  std::ifstream metadata(tmp);
+  metadata >> file_to_read;
+  metadata.close();
+  */
+  for(int i = 0; i < file_to_read; i++){
+    ::sprintf(tmp, "data/%c/%d.fdt", m_data.GetType(), i);
+    m_current_file_path = tmp;
+    m_current_file.open(m_current_file_path);
+    while(!m_current_file.eof()){
+      uint32_t magic_header = 0;
+      while (magic_header != 0xe1e01cca) {
+        m_current_file.read(reinterpret_cast<char *>(&magic_header), 
+                                               sizeof(magic_header));
+      }
+      data::frame_data tmp_frame_data;
+      m_current_file.read(reinterpret_cast<char *>(&tmp_frame_data.tsec), 
+                                             sizeof(tmp_frame_data.tsec));
+      m_current_file.read(reinterpret_cast<char *>(&tmp_frame_data.tusec), 
+                                             sizeof(tmp_frame_data.tusec));
+      m_current_file.read(reinterpret_cast<char *>(&tmp_frame_data.width), 
+                                             sizeof(tmp_frame_data.width));
+      m_current_file.read(reinterpret_cast<char *>(&tmp_frame_data.height), 
+                                             sizeof(tmp_frame_data.height));
+      uint32_t bytes;
+      m_current_file.read(reinterpret_cast<char *>(&bytes), sizeof(bytes));
+      m_current_file.read(reinterpret_cast<char *>(&tmp_frame_data.frame[0]), \
+                              tmp_frame_data.width * tmp_frame_data.height *  \
+                                             sizeof(tmp_frame_data.frame[0]));     
+    }
+    m_current_file.close();
+  }
+  return true;
 }
