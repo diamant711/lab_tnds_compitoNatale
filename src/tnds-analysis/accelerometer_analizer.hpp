aaf2464ccba40442b39405781e03d1845f1f7f75
diamant711 <53996598+diamant711@users.noreply.github.com>
2021-12-29 16:51:49 +0100

    Stared accelerometor_analizer (UNTESTED)

--- src/tnds-analysis/accelerometer_analizer.hpp
+++ src/tnds-analysis/accelerometer_analizer.hpp
@@ -1,5 +1,21 @@
 #include "common_type.hpp"
 
 class accelerometer_analizer {
+  public:
+    accelerometer_analizer(data &);
+    ~accelerometer_analizer();
+  
+    bool cook();
 
+  private:
+    data m_data;
 };
+
+accelerometer_analizer::accelerometer_analizer(data &dat) : m_data(dat) {}
+
+accelerometer_analizer::~accelerometer_analizer() {}
+
+bool accelerometer_analizer::cook() {
+  
+  return true;
+}
