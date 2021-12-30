49348dee63c3950eadba2d2ba0ac02c80e637ee6
diamant711 <53996598+diamant711@users.noreply.github.com>
2021-12-30 10:44:41 +0100

    Finished accelerometer_analizer (UNTESTED)

--- src/tnds-analysis/accelerometer_analizer.hpp
+++ src/tnds-analysis/accelerometer_analizer.hpp
@@ -1,6 +1,7 @@
 #include "common_type.hpp"
 
 #include <iostream>
+#include <cmath>
 
 class accelerometer_analizer {
   public:
@@ -23,6 +24,17 @@ accelerometer_analizer::accelerometer_analizer(data &dat) : m_data(dat) {
 accelerometer_analizer::~accelerometer_analizer() {}
 
 bool accelerometer_analizer::cook() {
-  
+  for(int i = 0; i < m_data.GetRawData.size(); ++i) {
+    data::analized tmp_analized;
+    tmp_analized.T_s = static_cast<double>(m_data.GetRawData()[i].tsec) + 
+                       static_cast<double>(m_data.GetRawData()[i].tusec) * 
+                       pow(10,-6);
+    tm_analized.point.x = 0;
+    uint32_t first_four_byte = m_data.GetRawData()[i].frame[0];
+    if(((first_four_byte & 0xFFFF0000) >> 2) == 0x5551)
+      std::cerr << "Espected identifier 0x5551xxxx. found: "
+                << ((first_four_byte & 0xFFFF0000) >> 2) << std::endl;
+    tm_analized.point.y = (first_four_byte & 0x0000FFFF);
+  }
   return true;
 }
